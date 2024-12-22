b293ef5ed384ed7dca1e189c6ff485b01fa4e823
irfan mohamed <mohamedirfank700@gmail.com>
2024-12-22 18:18:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ app.use(express.json());
 
 // Set the view engine to EJS
 app.set("view engine", "ejs");
-
+app.set('views', path.join(process.cwd(), 'views'));
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
